5d73b71c9901a9e6982e53f2edf3334c85ca3a95
gtoonstra <gtoonstra@gmail.com>
2015-02-14 11:25:55 -0300

    broker is building

--- broker/src/main.c
+++ broker/src/main.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
+#include "zhelpers.h"
 
-int main( int argc, char *argv[] )
+int main (int argc, char *argv[] )
 {
-    printf( "hello world!\n" );
+    void *context = (void *)zmq_ctx_new();
+    void *frontend = zmq_socket (context, ZMQ_XSUB);
+    assert (frontend);
+    int rc = zmq_bind (frontend, "tcp://*:5557");
+    assert( rc == 0 );
+    void *backend = zmq_socket (context, ZMQ_XPUB);
+    assert (backend);
+    rc = zmq_bind (backend, "tcp://*:5558");
+    assert( rc == 0 );
+
+    rc = zmq_proxy (frontend, backend, NULL);
+
+    // We never get here…
+    zmq_close (frontend);
+    zmq_close (backend);
+    zmq_ctx_destroy (context);
+    return 0;
 }
+
